b0f0ec5ca010be51e3f3f62934f0f2f40bb94ad7
Valles <eddvalles201@gmail.com>
2023-01-15 11:40:37 -0600

    Updated the models Book table to contain the publication_date field

--- Projects/myDjangoProject/bookr/reviews/models.py
+++ Projects/myDjangoProject/bookr/reviews/models.py
@@ -31,6 +31,9 @@ class Book(models.Model):
         max_length=70,
         help_text="The title of the book."
     )
+    publication_date = models.DateField(
+        verbose_name="Date the book was published."
+    )
     isbn = models.CharField(
         max_length=20,
         verbose_name="ISBN number of the book."
